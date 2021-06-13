da8f917ecbc45e52c8de066dfbba3d347cb1eb06
marcin.bazydlo <marcin.bazydlo@onwelo.com>
2021-06-13 18:06:38 +0200

    feat: v0.2.0 - Added option for checking returns

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from typing import Callable
+
 class TypesValidator:
-    def __init__(self, func):
-        self.func: function = func
-        self.annotations: dict = self.func.__annotations__
+    def __init__(self, func: Callable, check_return: bool=False):
+        self.func = func
+        self.annotations = self.func.__annotations__
+        self.check_return = check_return
 
     def __call__(self, *args, **kwargs):
 
@@ -9,11 +12,19 @@ class TypesValidator:
         error_messages.extend(self._args_parser(args))
         error_messages.extend(self._kwargs_parser(kwargs))
 
-        if error_messages:
+        if error_messages and not self.check_return:
             message = f"Function got {len(error_messages)} wrong inputs:\n" + "\n".join(error_messages)
             raise TypeError(message)
 
         result = self.func(*args, **kwargs)
+
+        if self.check_return:
+            if not isinstance(result, self.annotations['return']):
+                error_messages.append(f"Type of return is not correct. Should be {self.annotations['return']}, but got {result.__class__}")
+
+        if error_messages:
+            message = f"Function got {len(error_messages)} wrong inputs:\n" + "\n".join(error_messages)
+            raise TypeError(message)
 
         return result
 
@@ -40,5 +51,10 @@ class TypesValidator:
                 )
 
         return errors
+
+def validate_types(check_return: bool=False):
+    def _check(function):
+        return TypesValidator(function, check_return)
+    return _check
 
 
